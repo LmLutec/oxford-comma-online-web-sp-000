18a2e8492202160161fa982c280e7ba2697f79e2
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-17 23:22:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
- array.join(" and ")
+    if array.length == 1 
+      array.join 
+    elsif array.length == 2 
+     array.join(" and ")
+    elsif array.length == 3
+      array.join(" , ")
+    elsif array.length == 4
+      array.join(" , " + " and ")
+    end 
 end
