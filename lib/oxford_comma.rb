1093143e0b3d925c5a2760fa31a9430ef2145dd2
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 00:35:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     elsif array.length == 2 
      array.join(" and ")
     elsif array.length == 3
-      array.insert(0 , ",")
+      array.insert(1 , ",")
       array.insert(2 , "and")
       array.join(" ")
     elsif array.length> 3
