3a17abc4495abb10383ec2ae97cdd2a131ce5b26
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 00:24:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,7 @@ def oxford_comma(array)
     elsif array.length == 3
       array.insert(0 ,)
       array.insert(2 , "and")
+      array.join
     elsif array.length> 3
       array.insert(4, "and")
     end 
