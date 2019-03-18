e6bc733474c6cda4b2328f53ce96d149ec199b7d
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 00:48:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     elsif array.length == 2 
      array.join(" and ")
     elsif array.length == 3
-      array[2] << "and"
+      array[2]. "and"
       array.join(", ")
       
     elsif array.length> 3
